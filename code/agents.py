5790ad1530b05fe15906b38cd0fad0fbebc2d7f1
Julia Knoblauch <juliaknoblauch@t-online.de>
2021-07-28 16:22:07 +0200

    added weather_sel in load household again.probably didnt pull befor. sorry!

--- code/agents.py
+++ code/agents.py
@@ -1407,7 +1407,7 @@ class Performance_Evaluation_Agent:
         self.config = config
         self.weather_sel = weather_sel
         self.xai = xai
-        house_df = helper.load_household(DATA_PATH, config["data"]["household"])
+        house_df = helper.load_household(DATA_PATH, config["data"]["household"], weather_sel = weather_sel)
         if load_data:
             self.preparation = agents.Preparation_Agent(house_df)
         else:
