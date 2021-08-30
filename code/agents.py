8ff446637b276bdf928ee8f9ea96a3f587a44b0a
Julia Knoblauch <juliaknoblauch@t-online.de>
2021-08-30 20:04:21 +0200

    agents erweiter, shap jetzt richtig

--- code/agents.py
+++ code/agents.py
@@ -2020,8 +2020,9 @@ class X_Recommendation_Agent:
                     explaination_usage = self.Explainability_Agent.explanation_from_feature_importance_usage(feature_importance_usage_device, date=date, diagnostics=self.diagnostics)
                     print(explaination_usage)
 
-                    #hier if self.diagnostics == True
+
                     if self.diagnostics == True:
+                        print('Vizualizations for further insights into our predictions: ')
                         explainer_usage = recommendations_table['explainer_usage'].iloc[i]
                         shap_values_usage = recommendations_table['shap_values_usage'].iloc[i]
                         X_test_usage = recommendations_table['X_test_usage'].iloc[i]
@@ -2034,6 +2035,7 @@ class X_Recommendation_Agent:
             print(explaination_activity)
 
             if self.diagnostics == True:
+                print('Vizualizations for further insights into our predictions: ')
                 explainer_activity = recommendations_table[recommendations_table['device']=='Tumble Dryer']['explainer_activity'][0]
                 shap_values = recommendations_table[recommendations_table['device']=='Tumble Dryer']['shap_values'][0]
                 X_test_activity = recommendations_table[recommendations_table['device']=='Tumble Dryer']['X_test_activity'][0]
@@ -3120,7 +3122,7 @@ class Explainability_Agent:
         feature_names_activity = list(self.X_train_activity.columns.values)
         feature_names_activity
         # %%
-        vals_activity = np.abs(self.shap_values).mean(0)
+        vals_activity = self.shap_values[1]
         # %%
         feature_importance_activity = pd.DataFrame(list(zip(feature_names_activity, vals_activity)),
                                                    columns=['col_name', 'feature_importance_vals'])
@@ -3159,9 +3161,8 @@ class Explainability_Agent:
         feature_names_usage = list(self.X_train_usage.columns.values)
         feature_names_usage
         # %%
-        vals = np.abs(self.shap_values_usage).mean(0)
-        # vals = shap_values[1]
-        # %%
+        vals = self.shap_values_usage[1]
+
         feature_importance_usage = pd.DataFrame(list(zip(feature_names_usage, vals)),
                                                 columns=['col_name', 'feature_importance_vals'])
         feature_importance_usage.sort_values(by=['feature_importance_vals'], ascending=False, inplace=True)
@@ -3185,35 +3186,45 @@ class Explainability_Agent:
             active_past = 'not '
         else:
             active_past = ''
-            # input the activity lag with the strongest feature importance
-        FI_lag = np.argmax([feature_importance_activity.loc[feature_importance_activity['col_name']=='activity_lag_24','feature_importance_vals'],
-                           feature_importance_activity.loc[feature_importance_activity['col_name']=='activity_lag_48','feature_importance_vals'],
-                           feature_importance_activity.loc[feature_importance_activity['col_name']=='activity_lag_72','feature_importance_vals']])
-        if FI_lag == 0:
-            activity_lag = 'day'
-        elif FI_lag == 1:
-            activity_lag = 'two days'
-        elif FI_lag == 2:
-            activity_lag = 'three days'
+
+        # input the activity lag with the strongest feature importance
+        if feature_importance_activity.loc[feature_importance_activity['col_name']=='activity_lag_24','feature_importance_vals'].to_numpy()[0] >= 0 or \
+                feature_importance_activity.loc[feature_importance_activity['col_name']=='activity_lag_48','feature_importance_vals'].to_numpy()[0] >= 0 or \
+                feature_importance_activity.loc[feature_importance_activity['col_name']=='activity_lag_72','feature_importance_vals'].to_numpy()[0] >= 0:
+
+                FI_lag = np.argmax([feature_importance_activity.loc[feature_importance_activity['col_name']=='activity_lag_24','feature_importance_vals'],
+                                   feature_importance_activity.loc[feature_importance_activity['col_name']=='activity_lag_48','feature_importance_vals'],
+                                   feature_importance_activity.loc[feature_importance_activity['col_name']=='activity_lag_72','feature_importance_vals']])
+
+                if FI_lag == 0:
+                    activity_lag = 'day'
+                elif FI_lag == 1:
+                    activity_lag = 'two days'
+                elif FI_lag == 2:
+                    activity_lag = 'three days'
+                else:
+                    activity_lag = 'three days'
+
+                part1 = f"We believe you are active today since you were {active_past}active during the last {activity_lag}."
         else:
-            activity_lag = 'three days'
+            part1 = ""
 
         # weather:
         weather_hourly = pd.read_pickle('../export/weather_unscaled_hourly.pkl')
 
 
 
-        weather_array = np.array([feature_importance_activity.loc[feature_importance_activity[
-                                                                'col_name'] == 'dwpt', 'feature_importance_vals'],
-                                  feature_importance_activity.loc[feature_importance_activity[
-                                                                'col_name'] == 'rhum', 'feature_importance_vals'],
-                                  feature_importance_activity.loc[feature_importance_activity[
-                                                                'col_name'] == 'temp', 'feature_importance_vals'],
-                                  feature_importance_activity.loc[feature_importance_activity[
-                                                                      'col_name'] == 'wdir', 'feature_importance_vals'],
-                                  feature_importance_activity.loc[feature_importance_activity[
-                                                                      'col_name'] == 'wspd', 'feature_importance_vals']
-                                  ])
+        # weather_array = np.array([feature_importance_activity.loc[feature_importance_activity[
+        #                                                         'col_name'] == 'dwpt', 'feature_importance_vals'],
+        #                           feature_importance_activity.loc[feature_importance_activity[
+        #                                                         'col_name'] == 'rhum', 'feature_importance_vals'],
+        #                           feature_importance_activity.loc[feature_importance_activity[
+        #                                                         'col_name'] == 'temp', 'feature_importance_vals'],
+        #                           feature_importance_activity.loc[feature_importance_activity[
+        #                                                               'col_name'] == 'wdir', 'feature_importance_vals'],
+        #                           feature_importance_activity.loc[feature_importance_activity[
+        #                                                               'col_name'] == 'wspd', 'feature_importance_vals']
+        #                           ])
 
         d = {'features': ['dwpt', 'rhum', 'temp', 'wdir', 'wspd'],
              'labels': ['dewing point', 'relative humidity','temperature', 'wind direction', 'windspeed'],
@@ -3238,29 +3249,48 @@ class Explainability_Agent:
         df = pd.DataFrame(data=d)
 
         sorted_df = df['feature_importances'].sort_values(ascending=False)
-        weather1_ind = sorted_df.index[0]
-        weather1 = df['labels'][weather1_ind]
+        if sorted_df.iloc[0] >= 0:
+            weather1_ind = sorted_df.index[0]
+            weather1 = df['labels'][weather1_ind]
 
-        value1 = round(df['feature_values'][weather1_ind],2)
+            value1 = round(df['feature_values'][weather1_ind], 2)
 
-        weather2_ind = sorted_df.index[1]
-        weather2 = df['labels'][weather2_ind]
+            part2= f"The weather condition ({weather1}:{value1}) support that recommendation."
 
-        value2= round(df['feature_values'][weather2_ind],2)
+            if sorted_df.iloc[1] >= 0:
+
+                weather2_ind = sorted_df.index[1]
+                weather2 = df['labels'][weather2_ind]
+
+                value2 = round(df['feature_values'][weather2_ind], 2)
+                part2 = f"The weather conditions ({weather1}:{value1}, {weather2}:{value2}) support that recommendation."
+
+        else:
+            part2= ""
+
+        # Time features
+        # DAY
+        day_names = ['day_name_Monday','day_name_Tuesday','day_name_Wednesday','day_name_Thursday','day_name_Saturday','day_name_Sunday']
+        for day in day_names:
+            if feature_importance_activity.loc[feature_importance_activity['col_name'] == day, 'feature_importance_vals'].to_numpy()[0] >= 0:
+                part3 = "The weekday strenghtens that prediction."
+
+                if feature_importance_activity.loc[
+                    feature_importance_activity['col_name'] == 'hour', 'feature_importance_vals'].to_numpy()[0] >= 0:
+                    part3 = "The weekday and hour strenghtens that prediction."
+
+            else:
+                part3 = ""
+                if feature_importance_activity.loc[
+                    feature_importance_activity['col_name'] == 'hour', 'feature_importance_vals'].to_numpy()[0] >= 0:
+                    part3 = "The hour strenghtens that prediction."
+
 
         # final activity sentence
-        sentence_activity = (f"We believe you are active today since you were {active_past}active during the last {activity_lag}."
-                             f"The weather conditions ({weather1}:{value1}, {weather2}:{value2}) support that recommendation.")
+        sentence_activity = (str(part1) + str(part2)+ str(part3))
 
 
         explanation_sentence = sentence + sentence_activity
-        # optional vizualizations
-        if self.diagnostics == True:
-            print('Visualizations for further insights into our predictions: ')
-            #print('plot for activity')
-            # not tried yet (you have to run it in jupyter)
-            #shap_plot_activity = shap.force_plot(self.explainer_activity.expected_value[1], self.shap_values[1], self.X_test_activity.iloc[self.best_hour, :])
-            #display(shap_plot_activity)
 
 
         return explanation_sentence
@@ -3271,22 +3301,29 @@ class Explainability_Agent:
         self.diagnostics = diagnostics
 
         if self.X_test_usage['active_last_2_days'] == 0:
-            active_past = 'not '
+            active_past = 'not'
         else:
             active_past = ''
 
-        FI_lag = np.argmax([feature_importance_usage.loc[0, 'feature_importance_vals'],
-                            feature_importance_usage.loc[1, 'feature_importance_vals']])
+        if feature_importance_usage.loc[0, 'feature_importance_vals'] >= 0 or feature_importance_usage.loc[1, 'feature_importance_vals'] >= 0:
 
-        if FI_lag == 0:
-            device_usage = ""
-            number_days = 'day'
-        elif FI_lag == 1:
-            device_usage = ""
-            number_days = 'two days'
+            FI_lag = np.argmax([feature_importance_usage.loc[0, 'feature_importance_vals'],
+                                feature_importance_usage.loc[1, 'feature_importance_vals']])
+
+            if FI_lag == 0:
+                device_usage = ""
+                number_days = 'day'
+            elif FI_lag == 1:
+                device_usage = ""
+                number_days = 'two days'
+            else:
+                device_usage = " not"
+                number_days = 'two days'
+
+            part1 = f" and have{device_usage} used the device in the last {number_days}"
+
         else:
-            device_usage = "not "
-            number_days = 'two days'
+            part1= ""
 
         #weather:
         weather_daily = pd.read_pickle('../export/weather_unscaled_daily.pkl')
@@ -3315,28 +3352,28 @@ class Explainability_Agent:
         df = pd.DataFrame(data=d)
 
         sorted_df = df['feature_importances'].sort_values(ascending=False)
-        weather1_ind = sorted_df.index[0]
-        weather1 = df['labels'][weather1_ind]
-
-        value1 = round(df['feature_values'][weather1_ind],2)
-
-        weather2_ind = sorted_df.index[1]
-        weather2 = df['labels'][weather2_ind]
-
-        value2= round(df['feature_values'][weather2_ind],2)
 
 
-        sentence_usage = f"We believe you are likely to use the device in the near future since you were {active_past}active during the last 2 days and have {device_usage}used the device in " \
-                         f"the last {number_days}. " \
-                         f"The weather conditions ({weather1}:{value1}, {weather2}:{value2}) support that recommendation."
+        if sorted_df.iloc[0] >= 0:
+            weather1_ind = sorted_df.index[0]
+            weather1 = df['labels'][weather1_ind]
+
+            value1 = round(df['feature_values'][weather1_ind], 2)
+
+            part2= f"The weather condition ({weather1}:{value1}) support that recommendation."
+
+            if sorted_df.iloc[1] >= 0:
+
+                weather2_ind = sorted_df.index[1]
+                weather2 = df['labels'][weather2_ind]
+
+                value2 = round(df['feature_values'][weather2_ind], 2)
+                part2 = f"The weather conditions ({weather1}:{value1}, {weather2}:{value2}) support that recommendation."
+
+
+        sentence_usage = f"We believe you are likely to use the device in the near future since you " \
+                         f"were {active_past}active during the last 2 days" + str(part1) + "." + str(part2)
         explanation_sentence = sentence_usage
-
-        if self.diagnostics == True:
-            print('Vizualizations for further insights into our predictions: ')
-            #print('add plots here')
-            #shap_plot_usage = shap.force_plot(self.explainer_usage.expected_value[1], self.shap_values[1], self.X_test_usage)
-            #display(shap_plot_usage)
-            
 
         return explanation_sentence
 
